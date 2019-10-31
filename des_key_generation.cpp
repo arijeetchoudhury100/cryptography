26e4fc26f93f1c8f52bcc12569cb7cb2fc73ba0f
Arijeet Choudhury <40137186+arijeetchoudhury100@users.noreply.github.com>
2019-10-31 18:43:21 +0530

    Update and rename des.cpp to des_key_generation.cpp

--- des_key_generation.cpp
+++ des_key_generation.cpp
@@ -1,8 +1,7 @@
+//key generation for des algorithm
 #include<bits/stdc++.h>
 using namespace std;
 
-//void encrypt(bool*,bool[][48]);
-//void decrypt(bool*);
 void genkey(bool*,bool[][48]);
 void shiftleft(bool*,int);
 void combine(bool*,bool*,bool*);
@@ -24,31 +23,6 @@ int cdbox[48] = {14,17,11,24,1,5,3,28,
 		51,45,33,48,44,49,39,56,
 		34,53,46,42,50,36,29,32};
 
-/*int intialPermutation[64] = {58,50,42,34,26,18,10,2,
-			    60,52,44,36,28,20,12,4,
-			    62,54,46,38,30,22,14,6,
-			    64,56,48,40,32,24,16,8,
-			    57,49,41,33,25,17,9,1,
-			    59,51,43,35,27,19,11,3,
-			    61,53,45,37,29,21,13,5,
-			    63,55,47,39,31,23,15,7};
-
-int finalPermutation[64] = {40,8,48,16,56,24,64,32,
-			    39,7,47,15,55,23,63,31,
-			    38,6,46,14,54,22,62,30,
-			    37,5,45,13,53,21,61,29,
-			    36,4,44,12,52,20,60,28,
-			    35,3,43,11,51,19,59,27,
-			    34,2,42,10,50,18,58,26,
-			    33,1,41,9,49,17,57,25};
-int edbox = {32,1,2,3,4,5,
-	    4,5,6,7,8,9,
-	    8,9,10,11,12,13,
-	    12,13,14,15,16,17,
-	    16,17,18,19,20,21,
-	    20,21,22,23,24,25,
-	    24,25,26,27,28,29,
-	    28,29,31,31,32,1};*/
 int main(){
 	bool key[64]={0},msg[64],roundkey[16][48]={0};
 	char m[16],hexkeys[16][16]={0};
@@ -60,22 +34,7 @@ int main(){
 	//get key for 16 rounds in roundkey
 	genkey(key,roundkey);
 	converthex(roundkey,hexkeys);
-	//cout<<"enter the message(64bits):"<<endl;
-	//for(int i=0;i<64;i++)
-	//	cin>>msg[i];
-	//perform 16 rounds of encryption
-	//encrypt(msg,roundkey);
-	//cout<<"encrypted message is:"<<endl;
-	//for(int i=0;i<64;i++)
-	//	cout<<msg[i];
 	cout<<"the round keys are: "<<endl;
-	/*for(int i=0;i<16;i++){
-		cout<<"round: "<<i+1<<endl;
-		for(int j=0;j<48;j++){
-			cout<<roundkey[i][j]<<" ";
-		}
-		cout<<endl;
-	}*/
 	for(int i=0;i<16;i++){
 		for(int j=0;j<16;j++)
 			cout<<hexkeys[i][j];
@@ -83,35 +42,6 @@ int main(){
 	}
 	return 0;
 }
-/*void encrypt(bool* msg,bool roundkey[16][48]){
-	int temp[64];
-	bool left[32]={0},right[32]={0},tright[48]={0};
-	//perform initial permutation
-	for(int i=0;i<64;i++)
-		temp[i] = msg[i];
-	for(int i=0;i<64;i++)
-		msg[i] = temp[initialPermutation[i]-1];
-	//split the message into two halves 32-bit each
-	for(int i=0;i<32;i++)
-		left[i] = msg[i];
-	for(int i=32;i<64;i++);
-		right[i-32] = msg[i];
-	//perform 16 rounds
-	for(int i=0;i<16;i++){
-		//expand right half to 48 bits
-		for(int j=0;i<48;i++)
-			tright[j] = right[edbox[j]-1];
-		//perform XOR of right half and roundkey
-		for(int j=0;i<48;i++)
-			tright[j] = tright[j]^roundkey[i][j];
-		//compress 48bit result to 32 bits using 8 sboxes
-	}
-	//perform final permutation
-	for(int i=0;i<64;i++)
-		temp[i] = msg[i];
-	for(int i=0;i<64;i++)
-		msg[i] = temp[finalPemutation[i]-1];
-}*/
 void genkey(bool *key,bool roundkey[16][48]){
 	int num_shift[16]={1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
 	bool left[28],right[28];
